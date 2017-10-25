3bc8e8ad5f50c9d4a4e165a5adf3f3d6becd4fc3
madjidgo <madjidgo@gmail.com>
2017-10-25 17:51:52 +0200

    get in shape folders View/Template

---
+++ Views/Template/header.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html class="no-js" lang="">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+        <link rel="icon" type="png" href="car.png" />
+
+        <!-- Place favicon.ico in the root directory -->
+
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/main.css">
+        <script src="js/vendor/modernizr-2.8.3.min.js"></script>
+    </head>
+    <body>
+      <header class="jumbotron jumbotron-fluid mb-5 mainColor colorWhite">
+    <div class="container">
+      <h1 class="display-3">CarHandle</h1>
+      <p class="lead">Simplon</p>
+    </div>
+  </header>
